ca4f40a56b84fab96daa4abc89b7d8672ed2077b
Trung Nguyen Van <nguyentrungmx27@gmail.com>
2026-07-09 09:34:06 +0700

    Improve: Update files.

--- examples/fiber/handlers/user.go
+++ examples/fiber/handlers/user.go
@@ -6,10 +6,22 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	rest "github.com/otmc-sw/rest"
-	"github.com/otmc-sw/rest/examples/fiber/services"
 )
+
+type User struct {
+	ID    string
+	Name  string
+	Email string
+}
+
+type UserRequest struct {
+	Name  string
+	Email string
+}
 
 type UserResponse struct {
 	ID    string `json:"id"`
@@ -17,35 +29,67 @@ type UserResponse struct {
 	Email string `json:"email"`
 }
 
+func ValidateUser(r UserRequest) error {
+	return rest.Validate().
+		Required(r.Name).
+		Email(r.Email).
+		Validate()
+}
+
 func CreateUser(c *fiber.Ctx) error {
 	return rest.
-		Create[services.CreateUserRequest, services.User, UserResponse](FiberContext{Ctx: c}).
+		Create[UserRequest, User, UserResponse](FiberContext{Ctx: c}).
 		Bind().
-		Validate(services.Validates()).
-		Handle(services.CreateUserHandler()).
+		Validate(ValidateUser).
+		Handle(func(ctx rest.Context, req UserRequest) (User, error) {
+			user := User{
+				ID:    fmt.Sprintf("usr_%s", req.Email),
+				Name:  req.Name,
+				Email: req.Email,
+			}
+			return user, nil
+		}).
 		Respond()
 }
 
 func GetUser(c *fiber.Ctx) error {
 	return rest.
-		Get[struct{}, services.User, UserResponse](FiberContext{Ctx: c}).
-		Handle(services.GetUserHandler()).
+		Get[struct{}, User, UserResponse](FiberContext{Ctx: c}).
+		Handle(func(ctx rest.Context, req struct{}) (User, error) {
+			id := ctx.Param("id")
+			user := User{
+				ID:    id,
+				Name:  "John Doe",
+				Email: "john@example.com",
+			}
+			return user, nil
+		}).
 		Respond()
 }
 
 func UpdateUser(c *fiber.Ctx) error {
 	return rest.
-		Update[services.UpdateUserRequest, services.User, UserResponse](FiberContext{Ctx: c}).
+		Update[UserRequest, User, UserResponse](FiberContext{Ctx: c}).
 		Bind().
-		Validate(services.ValidatesUpdate()).
-		Handle(services.UpdateUserHandler()).
+		Validate(ValidateUser).
+		Handle(func(ctx rest.Context, req UserRequest) (User, error) {
+			id := ctx.Param("id")
+			user := User{
+				ID:    id,
+				Name:  req.Name,
+				Email: req.Email,
+			}
+			return user, nil
+		}).
 		Respond()
 }
 
 func DeleteUser(c *fiber.Ctx) error {
 	return rest.
 		Delete[UserResponse](FiberContext{Ctx: c}).
-		Handle(services.DeleteUserHandler()).
+		Handle(func(ctx rest.Context, req struct{}) (struct{}, error) {
+			ctx.Param("id")
+			return struct{}{}, nil
+		}).
 		Respond()
 }
-
